6e59d36c31b541bfcbfce8c1ffca32758ce70454
FaranRaja <faranraja011@gmail.com>
2026-03-30 18:50:44 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import Nav from './components/navbar/Nav'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      
+      <header>
+        <Nav />
+      </header>
+      
+    </div>
+  )
+}
+
+export default App
