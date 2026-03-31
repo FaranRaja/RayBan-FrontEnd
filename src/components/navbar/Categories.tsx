12a534925d2eb5c56a2199b5ec73e60224273788
FaranRaja <faranraja011@gmail.com>
2026-03-31 09:51:21 +0500

    starting nav

---
+++ src/components/navbar/Categories.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <ul className='flex items-center gap-5 font-bold text-white'>
+      <li>NEW IN</li>
+      <li>SUNGLASSES</li>
+      <li>EYEGLASSES</li>
+      <li className='text-[#e80c00]'>PROMO</li>
+    </ul>
+  )
+}
+
+export default Categories
--- src/components/navbar/Nav.tsx
+++ src/components/navbar/Nav.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Categories from './Categories'
 
 const Nav = () => {
   return (
-    <div>
+    <div className='bg-black flex items-center justify-between p-4'>
+
+      <img src="https://www.ray-ban.com/rbstatichtml/assets/images/icons/red-logo.svg" alt=""  className='w-20 h-20'/>
+
+      <Categories />
       
     </div>
   )
